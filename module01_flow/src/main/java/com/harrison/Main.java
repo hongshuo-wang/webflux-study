fc7b5795f497876f6d3d6fccbf3f2c64113942f7
WangHS <wanghongshuo@chinahuatong.com.cn>
2025-01-14 17:26:57 +0800

    add proccessor

--- module01_flow/src/main/java/com/harrison/Main.java
+++ module01_flow/src/main/java/com/harrison/Main.java
@@ -4,6 +4,38 @@ import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
 
 public class Main {
+	/**
+	 * 自定义处理器，既是发布者，又是订阅者
+	 */
+	static class MyProcessor extends SubmissionPublisher<String> implements Flow.Processor<String, String> {
+		private Flow.Subscription subscription;
+		@Override
+		public void onSubscribe(Flow.Subscription subscription) {
+			System.out.println(Thread.currentThread() + "---onSubscribe");
+			subscription.request(1);
+			// 绑定订阅关系
+			this.subscription = subscription;
+		}
+
+		@Override
+		public void onNext(String item) {
+			// 前缀加一个哈哈
+			String newItem = "哈哈" + item;
+			System.out.println(Thread.currentThread() + "---onNext: " + newItem);
+			submit(newItem);
+			subscription.request(1);
+		}
+
+		@Override
+		public void onError(Throwable throwable) {
+			System.out.println(Thread.currentThread() + "---onError：" + throwable.getMessage());
+		}
+
+		@Override
+		public void onComplete() {
+			System.out.println(Thread.currentThread() + "---onComplete");
+		}
+	}
 	public static void main(String[] args) throws InterruptedException {
 		// 定义一个发布者
 		SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
@@ -13,14 +45,14 @@ public class Main {
 			Flow.Subscription subscription;
 			@Override
 			public void onSubscribe(Flow.Subscription subscription) {
-				System.out.println(Thread.currentThread() + "---onSubscribe");
+				System.out.println(Thread.currentThread() + "---onSubscribe  ---processor");
 				this.subscription = subscription;
 				subscription.request(1);
 			}
 
 			@Override
 			public void onNext(String item) {
-				System.out.println(Thread.currentThread() + "---onNext: " + item);
+				System.out.println(Thread.currentThread() + "---onNext: " + item + "  ---processor");
 				// 取消订阅
 				// subscription.cancel();
 				subscription.request(1);
@@ -28,17 +60,19 @@ public class Main {
 
 			@Override
 			public void onError(Throwable throwable) {
-				System.out.println(Thread.currentThread() + "---onError：" + throwable.getMessage());
+				System.out.println(Thread.currentThread() + "---onError：" + throwable.getMessage() + "  ---processor");
 			}
 
 			@Override
 			public void onComplete() {
-				System.out.println(Thread.currentThread() + "---onComplete");
+				System.out.println(Thread.currentThread() + "---onComplete  ---processor");
 			}
 		};
 
+		MyProcessor myProcessor = new MyProcessor();
 		// 发布者订阅一个订阅者
-		publisher.subscribe(subscriber);
+		publisher.subscribe(myProcessor);
+		myProcessor.subscribe(subscriber);
 
 		// 发布数据
 		for(int i = 0; i < 10; i++) {
